2d5f04c674cf3a1570ea66e8881c7964b232c73b
uroven4 <68789163+uroven4@users.noreply.github.com>
2020-07-27 17:24:08 -0400

    Create hello.asm

---
+++ hello.asm
@@ -0,0 +1,23 @@
+; hello.asm 
+; assemble with "nasm -f bin -o hi.com hello.asm"
+
+    org  0x100        ; .com files always start 256 bytes into the segment
+
+    ; int 21h is going to want...
+
+    mov  dx, msg      ; the address of or message in dx
+    mov  ah, 9        ; ah=9 - "print string" sub-function
+    int  0x21         ; call dos services
+
+    mov  dl, 0x0d     ; put CR into dl
+    mov  ah, 2        ; ah=2 - "print character" sub-function
+    int  0x21         ; call dos services
+
+    mov  dl, 0x0a     ; put LF into dl
+    mov  ah, 2        ; ah=2 - "print character" sub-function
+    int  0x21         ; call dos services
+
+    mov  ah, 0x4c     ; "terminate program" sub-function
+    int  0x21         ; call dos services
+
+    msg  db 'Hello again, World!$'   ; $-terminated message
