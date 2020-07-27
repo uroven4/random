2c9c1207d620ca2001a6a5afe68089c303a107cd
uroven4 <68789163+uroven4@users.noreply.github.com>
2020-07-27 16:34:12 -0400

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("https://www.flickr.com/photos/189491566@N04/50160413911/");
+   printf("konnichi wa");
    return 0;
 }
